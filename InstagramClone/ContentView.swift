26972b8f2f002a3c4358447523699410f921ad78
Gustavo Carvalho <gstcarv@gmail.com>
2022-04-19 22:42:52 -0300

    Initial Commit

---
+++ InstagramClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramClone
+//
+//  Created by Gustavo Carvalho on 19/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Gustavo Carvalho on 19/04/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
