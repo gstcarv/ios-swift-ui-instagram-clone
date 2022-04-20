198b5cb31c4353aaf77b32a19b1f2439cc5627e1
Gustavo Carvalho <gstcarv@gmail.com>
2022-04-20 17:54:05 -0300

    feat: added NavigationView to the screens

--- InstagramClone/View/Main/MainTabView.swift
+++ InstagramClone/View/Main/MainTabView.swift
@@ -10,38 +10,42 @@ import SwiftUI
 struct MainTabView: View {
     
     var body: some View {
-        TabView {
-            FeedView()
-                .tabItem {
-                    Image(systemName: "house")
-                        .environment(\.symbolVariants, .none)
-                }
-            
-            SearchView()
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                        .environment(\.symbolVariants, .none)
-                }
-            
-            UploadPostView()
-                .tabItem {
-                    Image(systemName: "plus.square")
-                        .environment(\.symbolVariants, .none)
-                }
-            
-            NotificationView()
-                .tabItem {
-                    Image(systemName: "heart")
-                        .environment(\.symbolVariants, .none)
-                }
-            
-            ProfileView()
-                .tabItem {
-                    Image(systemName: "person")
-                        .environment(\.symbolVariants, .none)
-                }
+        NavigationView {
+            TabView {
+                FeedView()
+                    .tabItem {
+                        Image(systemName: "house")
+                            .environment(\.symbolVariants, .none)
+                    }
+                
+                SearchView()
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                            .environment(\.symbolVariants, .none)
+                    }
+                
+                UploadPostView()
+                    .tabItem {
+                        Image(systemName: "plus.square")
+                            .environment(\.symbolVariants, .none)
+                    }
+                
+                NotificationView()
+                    .tabItem {
+                        Image(systemName: "heart")
+                            .environment(\.symbolVariants, .none)
+                    }
+                
+                ProfileView()
+                    .tabItem {
+                        Image(systemName: "person")
+                            .environment(\.symbolVariants, .none)
+                    }
+            }
+            .accentColor(.black)
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .accentColor(.black)
     }
 }
 
